49d3f120d47a84f254e761331dd6a6bda4f6ad82
Pratham Sharma <prathamkssharma@gmail.com>
2026-04-02 17:31:22 +0530

    Emptying the repository

--- src/main/java/com/ecommerce/productcart/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.productcart.repository;
-
-import com.ecommerce.productcart.model.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
--- src/main/java/com/ecommerce/productcart/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.productcart.repository;
-
-import com.ecommerce.productcart.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
